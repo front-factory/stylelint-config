0c10753c7fc6e1386b798f8af27b846afaa6023f
Boris Damevin <borisdamevin@gmail.com>
2024-11-29 15:19:33 +0100

    feat: add rules

---
+++ index.js
@@ -0,0 +1,9 @@
+export default {
+    plugins: [
+        '@stylistic/stylelint-plugin'
+    ],
+    rules: {
+        '@stylistic/string-quotes': 'double',
+        '@stylistic/indentation': 4
+    }
+};
