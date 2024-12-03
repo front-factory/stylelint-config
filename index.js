5fe9685ceeff31375217aaa5a026317b4d42e6d4
Boris Damevin <borisdamevin@gmail.com>
2024-12-03 22:53:22 +0100

    feat(rules): add `at-rule-empty-line-before`

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ export default {
         '@stylistic/stylelint-plugin'
     ],
     rules: {
+        'at-rule-empty-line-before': [
+            'always',
+            {
+                'except': [
+                    'blockless-after-same-name-blockless',
+                    'first-nested'
+                ],
+                'ignore': [
+                    'after-comment'
+                ],
+                'ignoreAtRules': [
+                    'else'
+                ]
+            }
+        ],
         '@stylistic/string-quotes': 'double',
         '@stylistic/indentation': 4
     }
