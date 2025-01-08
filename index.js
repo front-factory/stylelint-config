6c20f49ec70c962273bb7a9cde7366cca1f3d089
Boris Damevin <borisdamevin@gmail.com>
2025-01-08 09:56:38 +0100

    feat(rules): add `scss/at-function-parentheses-space-before`

--- index.js
+++ index.js
@@ -31,9 +31,11 @@ export default {
         'scss/at-else-closing-brace-newline-after': 'always-last-in-chain',
         'scss/at-else-closing-brace-space-after': 'always-intermediate',
         'scss/at-else-empty-line-before': 'never',
+        'scss/at-function-parentheses-space-before': 'never',
         'scss/at-if-closing-brace-newline-after': 'always-last-in-chain',
         'scss/at-if-closing-brace-space-after': 'always-intermediate',
         'scss/at-mixin-argumentless-call-parentheses': 'always',
+        'scss/at-mixin-parentheses-space-before': 'never',
         'scss/dollar-variable-pattern': null,
         'scss/selector-no-redundant-nesting-selector': true
     }
